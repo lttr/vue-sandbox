7ecdf366285854d4a87083b5afad841fb2aca904
Lukas Trumm <lukas.trumm@gmail.com>
2024-01-08 08:31:43 +0100

    Play with reactive arrays

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,19 +3,20 @@
     <h2>Reactivity</h2>
 
     <section>
-        I have a <b>ref</b> <code>foo</code> with value <code>{{ foo }}</code>
-        <button @click="foo++">Increase foo</button>
+      I have a <b>ref</b> <code>foo</code> with value <code>{{ foo }}</code>
+      <button @click="foo++">Increase foo</button>
     </section>
 
     <section>
       I have a <b>ref</b> <code>bar</code> with an object
       <pre><code>{{ JSON.stringify(bar) }}</code></pre>
-      with source <pre><code>{{ JSON.stringify(sourceForBar) }}</code></pre>
+      with source
+      <pre><code>{{ JSON.stringify(sourceForBar) }}</code></pre>
       <button @click="bar.a++">Change bar's a</button>
       <button @click="bar.c.d++">Change bar's d</button>
       <button @click="sourceForBar.b++">Change bar's source b</button>
       <button @click="bar.c = { d: 12 }">Replace bar's c</button>
-      <button @click="bar = {a: 20, b: 40, c: { d: 3 }}">Replace bar completely</button>
+      <button @click="bar = { a: 20, b: 40, c: { d: 3 } }">Replace bar completely</button>
       <div><code>bar.a</code> is {{ bar.a }}</div>
       <div><code>a</code> destructured from bar is still {{ aFromBar }}</div>
       <div><code>c</code> destructred from bar is {{ destructredCFromBar }}</div>
@@ -26,12 +27,13 @@
     <section>
       I have a <b>reactive</b> <code>baz</code> with an object
       <pre><code>{{ JSON.stringify(baz) }}</code></pre>
-      with source <pre><code>{{ JSON.stringify(sourceForBaz) }}</code></pre>
+      with source
+      <pre><code>{{ JSON.stringify(sourceForBaz) }}</code></pre>
       <button @click="baz.a++">Change baz's a</button>
       <button @click="baz.c.d++">Change baz's d</button>
       <button @click="sourceForBaz.b++">Change baz's source b</button>
       <button @click="baz.c = { d: 12 }">Replace baz's c</button>
-      <button @click="baz = {a: 20, b: 40, c: { d: 3 }}">Replace baz completely</button>
+      <button @click="baz = { a: 20, b: 40, c: { d: 3 } }">Replace baz completely</button>
       <div><code>baz.a</code> is {{ baz.a }}</div>
       <div><code>a</code> destructured from baz is still {{ aFromBaz }}</div>
       <div><code>c</code> destructred from baz is {{ destructredCFromBaz }}</div>
@@ -39,11 +41,20 @@
       <div><code>toRef(baz, 'a')</code> is {{ aRefFromBaz }}</div>
     </section>
 
+    <section>
+      <div>
+        <code>{{ reactiveArray }}</code>
+      </div>
+      <div>
+        <code>{{ refArray }}</code>
+      </div>
+      <button @click="onChangeArray">Change something</button>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, toRef } from 'vue';
+import { ref, reactive, toRef, watchEffect } from 'vue'
 
 const foo = ref(0)
 
@@ -69,7 +80,19 @@ const aRefWrongFromBaz = toRef(baz.a)
 const aRefFromBaz = toRef(baz, 'a')
 const { c: destructredCFromBaz } = baz
 
-
+const reactiveArray = reactive(['a', 'b', 'c'])
+const refArray = ref(['d', 'e', 'f'])
+const something = ref(false)
+function onChangeArray() {
+  something.value = !something.value
+}
+watchEffect(() => {
+  const chars =
+    String.fromCharCode(Math.floor(Math.random() * 26) + 97) + something.value.toString()[0]
+  reactiveArray.unshift(chars)
+  // refArray.value.unshift(chars)
+  refArray.value = refArray.value.toSpliced(0, 0, chars)
+})
 </script>
 
 <style scoped>
